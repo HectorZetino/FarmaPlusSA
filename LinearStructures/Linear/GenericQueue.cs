d00b00a70a21ef23cbf6309e2cfd7fe382dbb154
Hector <h.rzc.zetino@gmail.com>
2019-02-20 10:31:33 -0600

    Agregar Librerias

---
+++ LinearStructures/Linear/GenericQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasLineales.Linear
+{
+    class GenericQueue<T>
+    {
+        private Nodo<T> Head;
+        private Nodo<T> tail;
+        private int count = 0;
+
+
+        public void Enqueue(T value)
+        {
+            Nodo<T> _newNodo = new Nodo<T>(value);
+            if (Head == null)
+            {
+                Head = _newNodo;
+                tail = Head;
+            }
+            else
+            {
+                tail.Siguiente = _newNodo;
+                tail = tail.Siguiente;
+            }
+            count++;
+        }
+
+        public int Dequeue()
+        {
+            if (Head == null)
+            {
+                throw new Exception("La cola esta vacia || Queue is Empty");
+            }
+            T result = (T)Head.Value;
+            Head = Head.Siguiente;
+            int resultado = Convert.ToInt32(result);
+            return resultado;
+
+        }
+    }
+
+}
---
+++ LinearStructures/Linear/Nodo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasLineales.Linear
+{
+    public class Nodo<T>
+    {
+        public Nodo(T value)
+        {
+            this.Value = value;
+        }
+
+        //Atributos
+        public T Value
+        {
+            set;
+            get;
+        }
+
+        public Nodo<T> Siguiente
+        {
+            set;
+            get;
+        }
+    }
+
+}
+
