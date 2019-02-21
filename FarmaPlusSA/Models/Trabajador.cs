58b0b0c6ff4011a3924cb2b8e81142b20202bb44
Hector <h.rzc.zetino@gmail.com>
2019-02-20 20:12:17 -0600

    Changes

---
+++ FarmaPlusSA/Models/Trabajador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FarmaPlusSA.Models
+{
+    public class Trabajador
+    {
+
+            public string Nombre { get; set; }
+
+            public string Cod_empleado { get; set; }
+
+            public double Total_horas_trabajadas { get; set; }
+
+            public string EstadiaOficina { get; set; }
+
+            public double Salario { get; set; }
+       
+    }
+}
