0df90c50dc097ce689fc90bea7bd3f101507a654
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2019-02-20 18:09:48 -0600

    Adding View Models Logic

---
+++ FarmaPlusSA/ViewModel/jornadasTrabajador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FarmaPlusSA.Models;
+
+namespace FarmaPlusSA.ViewModel
+{
+    public class jornadasTrabajador
+    {
+        public List<Trabajador> Trabajadores { get; set; }
+    }
+}
