81eb6acacc096a1cff3918d671be957f32094e16
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2019-02-20 18:01:00 -0600

    Adding Models

---
+++ FarmaPlusSA/Models/Trabajador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FarmaPlusSA.Models
+{
+    public class Trabajador
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public int horasTrabajadas { get; set; }
+        public int Salario { get; set; }
+        public Boolean estaEnOficina { get; set; }
+        public List<Citas> Citas { get; set; }
+    }
+}
